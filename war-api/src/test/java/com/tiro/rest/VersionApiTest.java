75a6bb96632b1da6a3aa0d14fd9480fde4924bfb
Oleksandr Kucherenko <kucherenko.alex@gmail.com>
2016-05-15 18:25:44 +0200

    FIX: grizzly2 logs to SLF4J bridge set

--- war-api/src/test/java/com/tiro/rest/VersionApiTest.java
+++ war-api/src/test/java/com/tiro/rest/VersionApiTest.java
@@ -3,17 +3,34 @@ package com.tiro.rest;
 import com.sun.jersey.test.framework.AppDescriptor;
 import com.sun.jersey.test.framework.JerseyTest;
 import org.junit.Test;
+import org.slf4j.bridge.SLF4JBridgeHandler;
 
 import javax.annotation.Nonnull;
+import java.util.Arrays;
+import java.util.logging.Handler;
+import java.util.logging.Logger;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 /** Jersey API testing. */
 public class VersionApiTest extends JerseyTest {
+  /** Trick: static variables initialized before the class members, we connect the SLF4J bridge for grizzly. */
+  private static final int TRICK_LOG_CONFIGURATION = configureLogger("", new SLF4JBridgeHandler());
+
+  private static int configureLogger(final String packageName, final java.util.logging.Handler handler) {
+    final Logger logger = Logger.getLogger(packageName);
+    final Handler[] handlers = logger.getHandlers();
+
+    Arrays.stream(handlers).forEach(logger::removeHandler);
+    logger.addHandler(handler);
+
+    return handlers.length;
+  }
 
   public VersionApiTest() {
     super(packageName(SecurityRestService.class));
   }
+
 
   public static String packageName(@Nonnull final Class<?> clazz) {
     return clazz.getPackage().getName();
