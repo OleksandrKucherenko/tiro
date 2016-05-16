ad202f1b8b0f15307b67cd2ecb67841b69101d01
Oleksandr Kucherenko <kucherenko.alex@gmail.com>
2016-05-16 22:17:32 +0200

    CHG: improved specifications

--- war-api/src/main/java/com/tiro/Services.java
+++ war-api/src/main/java/com/tiro/Services.java
@@ -1,14 +1,30 @@
 package com.tiro;
 
+import javax.ws.rs.core.MediaType;
+
 import static com.tiro.Services.Versions.V1;
 import static com.tiro.Services.Versions.V2;
 
 /** Constants for making managing of URL mapping easier. */
+@SuppressWarnings({"unused"})
 public interface Services {
 
+  /** High level API versions. */
   interface Versions {
+    /** First version. */
     String V1 = "/v1";
+    /** Second version often propose more speed optimized API. */
     String V2 = "/v2";
+  }
+
+  /** Supported DATA protocols. */
+  interface Protocols {
+    /** Protocol buffer. */
+    String PROTO = "application/x-protobuf";
+    /** JSON. */
+    String JSON = MediaType.APPLICATION_JSON;
+    /** Xml. */
+    String XML = MediaType.APPLICATION_XML;
   }
 
   /** Version 1 of security API. */
